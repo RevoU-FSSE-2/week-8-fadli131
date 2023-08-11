edd35bcc1ec6f0e193f8ac5b158664291037b2d2
fadli131 <fadliaryadinata011@gmail.com>
2023-08-11 16:23:10 +0700

    database revision

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const app = express();
-const port = 3001;
-
-app.get("/", (req, res) => {
-    res.send("Halo Semuanya");
-});
-
-app.listen(port, () =>{
-    console.log(`server listen ${port}`);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// import cors from 'cors';
+const body_parser_1 = __importDefault(require("body-parser"));
+const routes_1 = __importDefault(require("./routes"));
+const app = (0, express_1.default)();
+const port = 3000;
+// app.use(cors());
+app.use(body_parser_1.default.json());
+app.use('/api', routes_1.default);
+app.listen(port, () => {
+    console.log(`Server berjalan di 
+http://localhost/
+:${port}`);
 });
